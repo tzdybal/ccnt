04d1f76e82c6f30e3c82a5cce13274abbce704db
tzdybal <tomek@zdybal.lap.pl>
2016-01-30 19:01:22 +0100

    OpenCV - SimpleBlobDetector usage

--- main.cpp
+++ main.cpp
@@ -1,90 +1,47 @@
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d.hpp>
 #include <stdlib.h>
 #include <stdio.h>
 
 using namespace cv;
 
-/// Global variables
-
-Mat src, src_gray;
-Mat dst, detected_edges;
-
-int edgeThresh = 1;
-int lowThreshold;
-int const max_lowThreshold = 100;
-
-int minSize;
-int const max_minSize = 100;
-
-int ratio = 3;
-int kernel_size = 3;
-const char* window_name = "ccnt - Colony Counter";
-
-/**
- * @function CannyThreshold
- * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
- */
-void CannyThreshold(int, void*)
-{
-	/// Reduce noise with a kernel 3x3
-	blur( src_gray, detected_edges, Size(1,1) );
-
-	/// Canny detector
-	Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
-
-	std::vector<std::vector<cv::Point> > contours;
-	findContours(detected_edges, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-
-	Mat colonies = src.clone();
-
-	for (auto& contour : contours) {
-		double area = cv::contourArea(contour);
-		cv::Rect r = cv::boundingRect(contour);
-		int radius = std::max(r.width / 2, r.height / 2);
-		if (contour.size() > 6
-				&& radius > 3
-				&& radius < 30
-				&& std::abs(area / CV_PI*std::pow(radius, 2)) > 0.8) {
-			rectangle(colonies, r, Scalar(0, 255, 0));
-		}
-	}
-
-	/// Using Canny's output as a mask, we display our result
-	//dst = Scalar::all(0);
-
-	//src.copyTo( dst, detected_edges);
-	imshow( window_name, colonies );
-}
-
-
 /** @function main */
 int main( int argc, char** argv )
 {
 	/// Load an image
-	src = imread( argv[1] );
+	Mat src = imread( argv[1], IMREAD_GRAYSCALE );
 
 	if( !src.data )
 	{ return -1; }
 
-	/// Create a matrix of the same type and size as src (for dst)
-	dst.create( src.size(), src.type() );
+	// Set up the detector with default parameters.
+	SimpleBlobDetector::Params params;
+	params.filterByArea = true;
+	params.minArea = 40;
+	params.maxArea = 1000;
 
-	/// Convert the image to grayscale
-	cvtColor( src, src_gray, CV_BGR2GRAY );
+	params.minThreshold = 1;
+	params.thresholdStep = 1;
+	params.maxThreshold = 100;
 
-	/// Create a window
-	namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+	params.filterByColor = false;
+	params.filterByInertia = false;
+	params.filterByCircularity = true;
+	params.filterByConvexity = false;
+	params.minDistBetweenBlobs = 0.0f;
+	SimpleBlobDetector detector(params);
 
-	/// Create a Trackbar for user to enter threshold
-	createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
-	//createTrackbar( "Min size:", window_name, &minSize, max_minSize, CannyThreshold );
+	// Detect blobs.
+	std::vector<KeyPoint> keypoints;
+	detector.detect( src, keypoints);
 
-	/// Show the image
-	CannyThreshold(0, 0);
+	// Draw detected blobs as red circles.
+	// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
+	Mat im_with_keypoints;
+	drawKeypoints( src, keypoints, im_with_keypoints, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
 
-	/// Wait until user exit program by pressing a key
+	// Show blobs
+	imshow("keypoints", im_with_keypoints );
 	waitKey(0);
-
-	return 0;
 }
